16aa8bcf4e2b1b7e19d20e6d22e114b9f825deca
partcoin <32797605+partcoin@users.noreply.github.com>
2019-11-09 12:20:03 +0530

    Create oci_create_compartment.sh
    CLI shell script to create Compartment inside Oracle Cloud.

---
+++ oci_create_compartment.sh
@@ -0,0 +1,1 @@
+oci iam compartment create --compartment-id [OCID of ‘root’ compartment] --name cloud-native-microservices --description "A compartment for cloud native microservices" --region us-ashburn-1
